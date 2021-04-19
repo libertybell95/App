92f4d59cc73c5640bb13634a9d9b4ffaf8041a5b
thiennguyen00 <59074387+thiennguyen00@users.noreply.github.com>
2021-04-19 03:27:35 -0500

    Update Upload.js

--- src/Views/Upload.js
+++ src/Views/Upload.js
@@ -26,11 +26,36 @@ class Upload extends Component {
   };
   //::::::::::::::::::::::::::::::::
 
+  function csvJSON(csv){
+
+    var lines=csv.split("\n");
+
+    var result = [];
+
+    var headers=lines[0].split(",");
+
+    for(var i=1;i<lines.length;i++){
+
+  	  var obj = {};
+  	  var currentline=lines[i].split(",");
+
+  	  for(var j=0;j<headers.length;j++){
+  		  obj[headers[j]] = currentline[j];
+  	  }
+
+  	  result.push(obj);
+
+    }
+
+    //return result; //JavaScript object
+    return JSON.stringify(result); //JSON
+  }
+
   //::::::::: for putting all data in CSV to MySQL :::::::::::
   handleForce = (data, fileInfo) => {
     this.setState({ loading: true });
 
-    let d = JSON.stringify({data});
+    let d = csvJSON(data);
 
     fetch("https://maroonandwhiteserver.herokuapp.com/bulkcreate", {
       method: "POST",
