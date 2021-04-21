83066ad2d0a30e0a12b8b19321fc66b2fdcd924e
thiennguyen00 <59074387+thiennguyen00@users.noreply.github.com>
2021-04-21 16:19:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, static_folder='../build/', static_url_path='/')
+app = Flask(__name__, static_folder='build/', static_url_path='/')
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
